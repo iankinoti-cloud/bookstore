dbf41a7fe7b7a4763653ce14477e23fa2a64b14a
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-06-09 16:00:00 +0300

    feat: implement turn_page() method on Book class

--- lib/book.py
+++ lib/book.py
@@ -15,3 +15,6 @@ class Book:
             print("page_count must be an integer")
         else:
             self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
