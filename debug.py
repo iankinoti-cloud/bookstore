a4bbb88d6297bd95c1e1683eb172de99e25ef5e5
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-06-09 19:30:00 +0300

    refactor: update debug.py to import both Book and Coffee

--- debug.py
+++ debug.py
@@ -1,7 +1,9 @@
 import ipdb
-from lib.book import *
+from lib.book import Book
+from lib.coffee import Coffee
 
 cradle = Book("Cradle", 200)
+latte = Coffee(size="Large", price=3.50)
 
 if __name__ == '__main__':
     ipdb.set_trace()
