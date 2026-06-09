26283f8a9328d7ae2747889b7804db971350ef2d
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-06-09 12:30:00 +0300

    feat: add page_count property getter to Book class

--- lib/book.py
+++ lib/book.py
@@ -4,3 +4,7 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
